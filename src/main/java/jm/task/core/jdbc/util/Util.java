fc9d4ee029ecee873c5c8009a8fb6085ec48116e
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-09-13 18:16:12 +0300

    Добавлено подключение к бд.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Kata_PP";
+    private static Connection connection;
+    public static Connection getConnection () {
+            try {
+                if (!connection.isClosed()) {
+                    Driver driver = new com.mysql.cj.jdbc.Driver();
+                    DriverManager.registerDriver(driver);
+                    connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } finally {
+                try {
+                    connection.close();
+                }
+                catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        return connection;
+    }
 }
