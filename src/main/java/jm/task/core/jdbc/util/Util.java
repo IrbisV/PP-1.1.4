5aeaac38391345c33cb9916e96e400d077b1de8f
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-09-16 13:39:17 +0300

    настроено соединение c бд для hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class Util {
@@ -11,6 +19,7 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Kata_PP";
     private static Connection connection;
+    private static SessionFactory sessionFactory;
     public static Connection getConnection () {
             try {
                     Driver driver = new com.mysql.cj.jdbc.Driver();
@@ -20,5 +29,26 @@ public class Util {
                 throw new RuntimeException(e);
             }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydb");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "Kata_PP");
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "true");
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
     }
 }
