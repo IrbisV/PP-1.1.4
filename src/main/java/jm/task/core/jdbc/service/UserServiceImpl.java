90f7abbfc7f37dd57b863a6c050ffd3a910ff66a
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-09-16 14:59:29 +0300

    передаю реализацию из UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
@@ -9,7 +9,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     UserDao user;
     public UserServiceImpl () {
-        this.user = new UserDaoJDBCImpl();
+        this.user = new UserDaoHibernateImpl();
     }
     public void createUsersTable() {
         user.createUsersTable();
